dc6f2924b682fda01ae16e2b34b7d2caa57b6ff7
Peter Lockett <cerwym@googlemail.com>
2020-09-29 11:57:18 +0100

    fix: Skeleton loads style within index

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 
-import styles from '../styles/index.scss';
-
 interface IAppProps {
     message: string
 }
 
 export const App: React.FC<IAppProps> = ({message}) => {
     return (
-        <div className={styles.title}>
+        <div className='title'>
             <h1>Boundary Application</h1>
             {message}
         </div>
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {App} from "./components/app";
 
-import '/styles/index.scss'
+import './styles/index.scss'
 
 ReactDOM.render(
     <App message={'Hello World!'}/>,
