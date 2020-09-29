94f2501d48fae8a79ed72e29026568e96d1a681c
Peter Lockett <cerwym@googlemail.com>
2020-09-29 11:41:13 +0100

    deps: Added bootstrap, react-bootstrap, removed react-scripts

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import {App} from "./components/app";
 
+import '/styles/index.scss'
+
 ReactDOM.render(
     <App message={'Hello World!'}/>,
     document.getElementById('application')
